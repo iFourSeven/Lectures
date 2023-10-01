c7d25b5eb97fedf7785ae2ccb38a24f89749629d
Andrey Yurchak <andreyurchak@gmail.com>
2023-10-01 20:34:45 +0500

    hi name

---
+++ Exampels/exaple005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "andrey")
+{
+    Console.WriteLine("Ура, это же Andrey!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
