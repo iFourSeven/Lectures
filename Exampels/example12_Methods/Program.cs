080274640318c95c1c737b5dce432822a3d34761
Andrey Yurchak <andreyurchak@gmail.com>
2023-10-04 00:25:01 +0500

    ex12 methods

---
+++ Exampels/example12_Methods/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] arr = { 1, 2, 5, 0, 7, 6, 3, 4, 9, 8 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temprory = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temprory;
+    }
+}
+
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
